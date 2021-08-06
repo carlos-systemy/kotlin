b6bd7edfd5386c00f957feda48bcebb01c7e8abc
carlos <carlossilvafilho9@gmail.com>
2021-08-05 21:26:49 -0300

    array Double

---
+++ src/main/kotlin/onedigitalinnovetioncollections/TestDoubleArray.kt
@@ -0,0 +1,11 @@
+package onedigitalinnovetioncollections
+
+
+
+fun main() {
+    val salaries  = DoubleArray(size = 3)
+    salaries[0] = 1000.0
+    salaries[1] = 2000.0
+    salaries[2] = 3000.0
+     salaries.forEach { println(it)  }
+}
